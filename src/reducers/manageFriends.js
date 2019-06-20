4037b832756afb4ea4ba75f9ed79765f1e6ea1d9
minhl337 <imnotminh@gmail.com>
2019-06-20 11:57:14 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+    switch(action.type){
+        case'ADD_FRIEND':{
+            return {friends:[...state.friends,action.friend]}
+        }
+        case'REMOVE_FRIEND':{
+            let newArr = state.friends.filter(x=>x.id!=action.id)
+            return {friends:newArr}
+        }
+        default: return state
+    }
 }
